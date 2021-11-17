9fe900379a03ff492b066a9937f59361740f83d7
Serhii T <tocarchuk@gmail.com>
2021-11-17 09:58:26 +0200

    Fetch all info with API

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+const place = document.querySelector('.show-place');
+const img = document.querySelector('img');
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  return element;
-}
-
-document.body.appendChild(component());
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=London&appid=4d936a4be60b9f7812c9ca32648afb00`, {mode: 'cors'})
+.then(function(response) {
+  return response.json();
+})
+.then(function(response) {
+  console.log(response);
+  place.innerHTML = `
+  <span>${response.name}</span>
+  <br>
+  <span>${response.weather[0].description}</span>
+  <br>
+  <span>${response.main.temp} C</span>
+  <br>
+  <span>min: ${response.main.temp_min} C, max: ${response.main.temp_max} C</span>
+  `;
+})
