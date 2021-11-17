e1676e907e2a2fcbe088bc3a877d89ea43e9c4ad
Serhii T <tocarchuk@gmail.com>
2021-11-17 12:15:17 +0200

    Search place and show weather dynamically

--- src/index.js
+++ src/index.js
@@ -2,21 +2,26 @@ import _ from 'lodash';
 import './style.css';
 
 const place = document.querySelector('.show-place');
-const img = document.querySelector('img');
+const selectedPlaceInput = document.getElementById('search-place');
 
-fetch(`https://api.openweathermap.org/data/2.5/weather?q=London&appid=4d936a4be60b9f7812c9ca32648afb00`, {mode: 'cors'})
-.then(function(response) {
-  return response.json();
-})
-.then(function(response) {
-  console.log(response);
-  place.innerHTML = `
-  <span>${response.name}</span>
-  <br>
-  <span>${response.weather[0].description}</span>
-  <br>
-  <span>${response.main.temp} C</span>
-  <br>
-  <span>min: ${response.main.temp_min} C, max: ${response.main.temp_max} C</span>
-  `;
-})
+const searchPlaceBtn = document.getElementById('search');
+searchPlaceBtn.addEventListener('click', showWeather);
+
+function showWeather() {
+  let selectedPlace = selectedPlaceInput.value
+  fetch(`https://api.openweathermap.org/data/2.5/find?q=${selectedPlace}&units=metric&appid=4d936a4be60b9f7812c9ca32648afb00`, {mode: 'cors'})
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(response) {
+    console.log(response);
+    place.innerHTML = `
+    <span>${response.list[0].name}</span>
+    <br>
+    <span>${response.list[0].weather[0].description}</span>
+    <br>
+    <span>${response.list[0].main.temp} C</span>
+    <br>
+    <span>min: ${response.list[0].main.temp_min} C, max: ${response.list[0].main.temp_max} C</span>`;
+  })
+}
