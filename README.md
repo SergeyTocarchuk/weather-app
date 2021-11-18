1590b43a0cf8536033b5f20156acbc46e11849c0
Serhii T <tocarchuk@gmail.com>
2021-11-18 13:57:07 +0200

    Get Dates and convert them into day of week for forecast section

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 todo:
-- fetch the icon url;
-- add 7-days forecast in below section;
+- add 5-days forecast in below section;
+
+  // convert short day name to full day name
+  if (dayOfWeek === 'Mon') {
+    dayOfWeek = 'Monday';
+  } else if (dayOfWeek === 'Tue') {
+    dayOfWeek = 'Tuesday';
+  } else if (dayOfWeek === 'Wed') {
+    dayOfWeek = 'Wednesday';
+  } else if (dayOfWeek === 'Thu') {
+    dayOfWeek = 'Thursday';
+  } else if (dayOfWeek === 'Fri') {
+    dayOfWeek = 'Friday';
+  } else if (dayOfWeek === 'Sat') {
+    dayOfWeek = 'Saturday';
+  } else if (dayOfWeek === 'Sun') {
+    dayOfWeek = 'Sunday';
+  }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import _ from 'lodash';
 import './style.css';
 
 const currWeather = document.querySelector('.cur-weather');
+const forecast = document.querySelector('.forecast');
 
 const searchPlaceBtn = document.getElementById('search');
 searchPlaceBtn.addEventListener('click', showWeather);
@@ -15,21 +16,81 @@ function handleSearchInput(e) {
   }
 }
 
+function clearInput() {
+  selectedPlaceInput.value = "";
+  }
+
 function showWeather() {
-  let selectedPlace = selectedPlaceInput.value
+  let selectedPlace = selectedPlaceInput.value;
   fetch(`https://api.openweathermap.org/data/2.5/find?q=${selectedPlace}&units=metric&appid=4d936a4be60b9f7812c9ca32648afb00`, {mode: 'cors'})
   .then(function(response) {
     return response.json();
   })
   .then(function(response) {
-    console.log(response);
     currWeather.innerHTML = `
-    <span class="place">${response.list[0].name}</span>
-    <div class="weather-img">
-      <span class="current-temp">${Math.floor(response.list[0].main.temp)} °</span>
-      <img src="http://openweathermap.org/img/wn/${response.list[0].weather[0].icon}@2x.png">
-    </div>
-    <span>${response.list[0].weather[0].description}</span>
-    <span>min: ${Math.floor(response.list[0].main.temp_min)} °, max: ${Math.floor(response.list[0].main.temp_max)} °</span>`;
+      <span class="place">${response.list[0].name}</span>
+      <div class="weather-img">
+        <span class="current-temp">${Math.floor(response.list[0].main.temp)} °</span>
+        <img src="http://openweathermap.org/img/wn/${response.list[0].weather[0].icon}@2x.png">
+      </div>
+      <span>${response.list[0].weather[0].description}</span>
+      <span>min: ${Math.floor(response.list[0].main.temp_min)} °, max: ${Math.floor(response.list[0].main.temp_max)} °</span>`;
   })
+  .catch(error => {
+    console.error('There has been a problem with selected place:', error);
+  })
+  showForecast();
+  clearInput();
+}
+
+function showForecast() {
+  let selectedPlace = selectedPlaceInput.value;
+  fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${selectedPlace}&units=metric&appid=4d936a4be60b9f7812c9ca32648afb00`, {mode: 'cors'})
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(response) {
+    console.log(response);
+    forecast.innerHTML = `
+      <span>${convertDateToDay(response.list[0].dt_txt)}</span>
+      <span>${Math.floor(response.list[0].main.temp_max)} °</span>
+      <span>${Math.floor(response.list[4].main.temp_min)} °</span>
+      
+      <span>${convertDateToDay(response.list[8].dt_txt)}</span>
+      <span>${Math.floor(response.list[8].main.temp_max)} °</span>
+      <span>${Math.floor(response.list[12].main.temp_min)} °</span>
+      
+      <span>${convertDateToDay(response.list[16].dt_txt)}</span>
+      <span>${Math.floor(response.list[16].main.temp_max)} °</span>
+      <span>${Math.floor(response.list[20].main.temp_min)} °</span>
+      
+      <span>${convertDateToDay(response.list[24].dt_txt)}</span>
+      <span>${Math.floor(response.list[24].main.temp_max)} °</span>
+      <span>${Math.floor(response.list[28].main.temp_min)} °</span>
+      
+      <span>${convertDateToDay(response.list[32].dt_txt)}</span>
+      <span>${Math.floor(response.list[32].main.temp_max)} °</span>
+      <span>${Math.floor(response.list[36].main.temp_min)} °</span>`;
+  })
+}
+
+function convertDateToDay(date) {
+  let day = new Date(date).toString().split(' ')[0];
+
+  if (day === 'Mon') {
+    day = 'Monday';
+  } else if (day === 'Tue') {
+    day = 'Tuesday';
+  } else if (day === 'Wed') {
+    day = 'Wednesday';
+  } else if (day === 'Thu') {
+    day = 'Thursday';
+  } else if (day === 'Fri') {
+    day = 'Friday';
+  } else if (day === 'Sat') {
+    day = 'Saturday';
+  } else if (day === 'Sun') {
+    day = 'Sunday';
+  }
+  return day;
 }
